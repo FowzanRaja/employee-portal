161d1a2edfb3d1cd3ec0622891c25593d3d32782
Fowzan Raja <84584620+FowzanRaja@users.noreply.github.com>
2026-03-14 17:59:25 +0000

    Added routing

--- frontend/src/app/routes.jsx
+++ frontend/src/app/routes.jsx
@@ -1,11 +1,21 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import NotFound from '../pages/NotFound'
 import TestPage from '../pages/TestPage'
+import DashboardPage from '../pages/DashboardPage'
+import AnnouncementsPage from '../pages/AnnouncementsPage'
+import ConsultantsPage from '../pages/ConsultantsPage'
+import LoginPage from '../pages/LoginPage'
+import TicketPage from '../pages/TicketPage'
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/test" replace />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/announcements" element={<AnnouncementsPage />} />
+      <Route path="/consultants" element={<ConsultantsPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/ticket" element={<TicketPage />} />
       <Route path="/test" element={<TestPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
