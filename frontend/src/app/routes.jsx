b01c9ac56c8216a97860e997f3715112278c0e90
Fowzan Raja <84584620+FowzanRaja@users.noreply.github.com>
2026-03-17 08:21:34 +0000

    Dashboard v1

--- frontend/src/app/routes.jsx
+++ frontend/src/app/routes.jsx
@@ -1,22 +1,33 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import NotFound from '../pages/NotFound'
-import TestPage from '../pages/TestPage'
-import DashboardPage from '../pages/DashboardPage'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import AppShell from './AppShell'
 import AnnouncementsPage from '../pages/AnnouncementsPage'
 import ConsultantsPage from '../pages/ConsultantsPage'
+import DashboardPage from '../pages/DashboardPage'
+import LeavePage from '../pages/LeavePage'
 import LoginPage from '../pages/LoginPage'
+import MessagesPage from '../pages/MessagesPage'
+import NotFound from '../pages/NotFound'
+import PolicyPage from '../pages/PolicyPage'
 import TicketPage from '../pages/TicketPage'
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/test" replace />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/announcements" element={<AnnouncementsPage />} />
-      <Route path="/consultants" element={<ConsultantsPage />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/ticket" element={<TicketPage />} />
-      <Route path="/test" element={<TestPage />} />
+      <Route path="/test" element={<Navigate to="/" replace />} />
+      <Route path="/ticket" element={<Navigate to="/tickets" replace />} />
+
+      <Route path="/" element={<AppShell />}>
+        <Route index element={<DashboardPage />} />
+        <Route path="dashboard" element={<Navigate to="/" replace />} />
+        <Route path="tickets" element={<TicketPage />} />
+        <Route path="policies" element={<PolicyPage />} />
+        <Route path="leave" element={<LeavePage />} />
+        <Route path="announcements" element={<AnnouncementsPage />} />
+        <Route path="messages" element={<MessagesPage />} />
+        <Route path="consultants" element={<ConsultantsPage />} />
+      </Route>
+
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -1,3 +1,5 @@
+import Dashboard from '../components/Dashboard'
+
 export default function DashboardPage() {
-  return <div>Dashboard</div>
+  return <Dashboard />
 }
