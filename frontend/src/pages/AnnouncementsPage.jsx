02aa610418ec4540e2dd82c8c66f3dcfc9235a04
Lin <143440052+Linkc0@users.noreply.github.com>
2026-03-22 19:10:25 +0000

    Added pfp's to Announcement Page
    Modified Announcement Card to have a pfp section. Modified Announcement Page to include pfp in example announcements.  Also modified index css file to add css for pfps.

--- frontend/src/pages/AnnouncementsPage.jsx
+++ frontend/src/pages/AnnouncementsPage.jsx
@@ -1,9 +1,14 @@
 import AnnouncementCard from '../components/AnnouncementCard';
+import pfp1 from '../assets/pfp images/pfp1.jpg';
+import pfp2 from '../assets/pfp images/pfp2.jpg';
+import pfp3 from '../assets/pfp images/pfp3.jpg';
+
 
 const announcements = [
   {
     title: 'Test 1',
     name: 'HR Department',
+    profilePic: pfp3,
     date: 'March 15 2026',
     content:
       'Testing the announcement card component. This is a sample announcement to demonstrate the layout and styling of the announcement cards on the Announcements page. The content can be of varying length and will adjust accordingly.',
@@ -11,6 +16,7 @@ const announcements = [
   {
     title: 'Test 2',
     name: 'Larry Barry',
+    profilePic: pfp1,
     date: 'March 6 2026',
     content:
       'Join us at brunch this Wednesday at 12pm to celebrate sucessfully finishing the FDM portal. ',
@@ -18,6 +24,7 @@ const announcements = [
   {
     title: 'Test 3',
     name: 'Dr Farry Marry',
+    profilePic: pfp2,
     date: 'March 2, 2026',
     content:
       'Testing whether this announcement will show up at the bottom or the top of the page.',
@@ -49,6 +56,7 @@ export default function AnnouncementsPage() {
                 key={index}
                 title={a.title}
                 name={a.name}
+                profilePic={a.profilePic}
                 date={a.date}
                 content={a.content}
               />
